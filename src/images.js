ec10d8ad49aef4548179bd9c5c420fa46becd0d9
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-12-06 17:52:24 -0500

    Added more images

--- src/images.js
+++ src/images.js
@@ -1,4 +1,186 @@
 export default {
   SAMURAI:
-    "https://vignette.wikia.nocookie.net/talmanwha/images/2/2a/Blog_-_Imae.png/revision/latest?cb=20170706104125"
+    "https://vignette.wikia.nocookie.net/talmanwha/images/2/2a/Blog_-_Imae.png/revision/latest?cb=20170706104125",
+  GOLDEN_GIRLS:
+    "https://s.abcnews.com/images/Entertainment/golden-girls-group-h-getty-gty-jc-180726_hpMain_16x9_992.jpg",
+  OLD_AGE_HOME:
+    "http://prudhvisimhagroup.com/wp-content/uploads/2018/04/old-age-home.jpg",
+  FIFTY_SHADES:
+    "http://images6.fanpop.com/image/photos/40200000/Fifty-Shades-Of-Grey-Wallpaper-randomna12-40231694-1920-1080.jpg",
+  AMERICAN_PIE:
+    "http://blog.southernoutdoorcinema.com/wp-content/uploads/2014/10/banner-american-pie.png",
+  WATER:
+    "https://www.thegoldenspace.com.my/wp-content/uploads/2017/10/pi-butterfly.jpg",
+  BREATHE_IN:
+    "https://ehonami.blob.core.windows.net/media/2015/04/breathe-fresh-air-800x600.jpg",
+  BRITA:
+    "https://i5.walmartimages.ca/images/Enlarge/425/567/999999-60258425567.jpg",
+  PURIFIER:
+    "https://brain-images-ssl.cdn.dixons.com/1/3/10151731/l_10151731_006.jpg",
+  GIRL_IN_BED:
+    "http://www.dreams.co.uk/sleep-matters-club/wp-content/uploads/2017/04/Waking-in-the-night.jpg",
+  MARTIN_LUTHER_KING:
+    "https://cdn-images-1.medium.com/max/2000/1*l87Isvyxt2xtOgPS0AEYYg.jpeg",
+  DREAM_CATCHER:
+    "https://www.success.com/wp-content/uploads/legacy/sites/default/files/main/articles/stepstoachieveyourdream.jpg",
+  MOON_IN_NIGHT_SKY:
+    "http://www.cdte-rhone.com/wp-content/uploads/2017/09/dsfsdfsdfsdf.jpg",
+  ICEBERG:
+    "https://images.tech.co/wp-content/uploads/2018/05/04124009/What-Is-The-Deep-Web.jpg",
+  GIRL_UNDERWATER:
+    "https://static1.squarespace.com/static/58b887b6893fc05b37818218/58f538f3be6594ba21a3094a/59f4330f41920221548dee93/1513828111830/karim-iliya-girl-underwater-sunset.jpg?format=1000w",
+  DEEP_HOLE:
+    "http://www.exercisinginnerpeas.com/wp-content/uploads/2015/06/deep-hole.jpg",
+  GIRL_MEASURING_STUFF: "https://i.ytimg.com/vi/RDE4SlYdn5E/maxresdefault.jpg",
+  AMPITHEATRE:
+    "https://whc.unesco.org/uploads/thumbs/site_0091_0061-750-0-20140709164150.jpg",
+  OLD_RUINS:
+    "https://www.cornwalls.co.uk/sites/default/files/photos/Chyauster.jpg",
+  OLD_TELEPHONE: "https://i.ytimg.com/vi/u0rGMxJeFm0/maxresdefault.jpg",
+
+  GUY_LOOKING_FRUSTRATED:
+    "http://home.bt.com/images/6-medical-reasons-why-you-might-be-feeling-tired-all-the-time-136425477714502601-180228094029.jpg",
+  GUY_WIPING_EYES:
+    "https://www.rd.com/wp-content/uploads/2017/04/01_tired_Got-Eye-Strain-How-to-Keep-Screen-Time-for-Wrecking-Your-Vision_370347539_g-stockstudio-760x506.jpg",
+  GIRL_YAWNING:
+    "https://d34gx5qo4rcf53.cloudfront.net/blog/wp-content/uploads/2017/08/01153554/636237253164684571-1716217148_woman-yawning-contagious.jpg",
+  BABY_SLEEPING_AND_EATING:
+    "https://www.omicsgroup.org/articles-admin/disease-images/sleeprelated-eating-disorder-79712.jpg",
+  HOLDING_HANDS:
+    "https://media.swncdn.com/cms/BST/54863-28515-couple-praying-together.1200w.tn.800w.tn.jpg",
+  LITTLE_GIRL_LYING_DOWN:
+    "https://cdn.shopify.com/s/files/1/2118/9337/products/Comfort_U_Pillow_B_480x480.jpg?v=1501248415",
+  FOOT_SOLES:
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1076/shoeinsoles-1494518677.jpg?resize=480:*",
+  PILLOWS:
+    "https://cdn11.bigcommerce.com/s-b69zv4dehd/images/stencil/608x608/products/120/658/plush_pillow_2__03287.1535480008.PNG?c=2",
+  AMERICAN_NINJA_WARRIOR:
+    "http://www.streetfoodcinema.com/wp-content/uploads/2016/08/American-Ninja-Warrior-Yoast-SEO-Photo-1-1200x630.png",
+  STONES:
+    "https://images.pexels.com/photos/235990/pexels-photo-235990.jpeg?auto=compress&cs=tinysrgb&h=350",
+
+  CRYSTALS:
+    "https://cdn.shopify.com/s/files/1/0843/9839/files/amethyst_caves_970x.jpg?v=1528183798",
+  GUY_ON_MOUNTAIN:
+    "http://searchoflife.com/wp-content/uploads/2013/10/strong-willpower-best-medicine.jpg",
+
+  GIRL_PLAYING_WITH_DOG:
+    "http://3.bp.blogspot.com/-igMESeE_CdM/VeRIkQuMRcI/AAAAAAAAZyI/xbs3SycVwg0/s1600/cute-babies-playing-together-hd-wallpapers.jpg",
+  MEN_SHAKING_HANDS:
+    "http://find-way.net/wp-content/uploads/2016/03/hand-shaking.jpg",
+  GIRLS_SHAKING_HANDS:
+    "https://www.verywellfamily.com/thmb/CZgdHR1pbeoOiwOTTqcl6VxYQU8=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/Getty_girls_shake_hands_greeting-56a13dac3df78cf77268b35e.jpg",
+  PEOPLE_GREETING:
+    "http://learnenglish.vn/wp-content/uploads/2014/11/How-To-Introduce-People.jpg",
+
+  BOOKS:
+    "https://www.thefix.com/sites/default/files/styles/article/public/lessonslearned.jpg",
+  PROFESSOR_IN_CLASS:
+    "https://www.education.gov.gy/web/media/k2/items/cache/f1b8aeeb90e87d86cd6bf72988291f00_XL.jpg",
+  FATHER_TALKING_TO_SON:
+    "http://static.oprah.com/images/own/2013/lifeclass/20130505-lifeclass-fatherless-sons-15-600x411.jpg",
+  MOTHER_SCOLDING_DAUGHTER:
+    "https://s-i.huffpost.com/gen/1764264/images/n-MOTHER-SCOLDING-CHILDREN-IN-BLACK-AND-WHITE-628x314.jpg",
+  CHEF:
+    "http://www.3qrecruitment.ie/wp-content/uploads/2018/06/why-you-should-be-a-chef-in-dublin-by-three-q-recruitment-for-catering-780x430.jpg",
+  SHREDDING_CARROTS:
+    "https://cambridgewords.files.wordpress.com/2017/08/whats-cooking.jpg",
+  CUTTING_MEAT:
+    "https://www.betterhealth.vic.gov.au/-/media/bhc/images/slideshow/cut-down-on-fat/lean_meat-1050x590_istock_000023404881small.png",
+  FOOD:
+    "http://blogs-images.forbes.com/annabel/files/2014/10/Durga4_ChennaiIndia-e1414423822906-1152x647.jpg",
+  COLOURFUL_BIRD:
+    "http://smileforthebirdie.com/images/favorites%202014%20011.jpg",
+  RARE_SNAKE:
+    "https://notable-facts.com/wp-content/uploads/2018/01/Rare-and-Exotic-Animals-696x435.jpg",
+  TROPICAL_ISLAND:
+    "http://www.travelstart.com.ng/blog/wp-content/uploads/2014/03/Mauritius.jpg",
+  EXPENSIVE_CAR:
+    "https://www.arabianbusiness.com/sites/default/files/styles/full_img/public/images/2015/12/28/most+exotic+cars+of+20159.jpg",
+  FRESH_GOURMET:
+    "https://www.francemotorhomehire.com/wp-content/uploads/2017/12/gourmet-food.jpg",
+  CHEF_PREPARING_CAKE:
+    "https://joseyphina.files.wordpress.com/2016/03/wp-1458219117116.jpeg?w=1000",
+  CHOCOLATE_CAKE:
+    "https://az826390.vo.msecnd.net/cdn/media/home/inspiring_recipes/recipes/new_-_c/couronne-gourmande-chocolat-noir-1160x650-bs007437-pub-67290-01.ashx?la=en&mw=1160&w=1160&hash=9B81EF909280D8073F0B4DDF77C88E33A3DD72DE",
+  PASTURE:
+    "https://cdn.pixabay.com/photo/2017/10/08/01/39/open-fields-2828596_960_720.jpg",
+  OLD_CAR: "https://timelimo.com/wp-content/uploads/2011/03/1948-Bently.jpg",
+  MODEL_GIRL:
+    "https://scattergorieslists.com/wp-content/uploads/2017/07/green-eyes.jpg",
+  PAPER_HOUSE:
+    "https://www.envision-creative.com/wp-content/uploads/2017/08/GreenMarketing-e1503587297992.jpg",
+
+  FOOD2:
+    "https://supervalu.ie/thumbnail/720x400/var/files/good-food-karma/recipe/tasty-toastie-recipe.jpg?fill=1",
+
+  SLICE_OF_CHOCOLATE_CAKE:
+    "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX2911271.jpg",
+
+  ICE_CREAM_DISH:
+    "https://us.123rf.com/450wm/szefei/szefei1510/szefei151000115/46958672-top-view-chocolate-ice-cream-with-wafer-and-spoon-on-plate-table-cloth-background-.jpg?ver=6",
+  MACARONI_BOWL:
+    "https://s3.amazonaws.com/secretsaucefiles/photos/images/000/103/294/large/easy-mac.jpg?1485309295",
+  COUPLE_SLEEPING:
+    "https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/09/10/17/web-spooning-RF-corbis-v2.jpg?w968h681",
+  PLATE_AND_UTENSILS:
+    "http://flatheadliving.com/wp-content/uploads/2015/10/Basic_Setting_Fotolia.gif",
+  BABY_IN_SACK:
+    "https://www.healthreflect.com/webroot/img/articles/1512486231Too_Much_Amniotic_Fluid_Polyhydramnios_thumb.png",
+  GLASS_OF_WATER:
+    "https://www.healthline.com/hlcmsresource/images/high-blood-pressure-infographic/766x415_Diuretics.jpg",
+  ENGINE_OIL:
+    "http://townsquare.media/site/757/files/2014/04/changing-engine-fluid.jpg?w=980&q=75",
+  OBAMA_SWEATING:
+    "https://typeset-beta.imgix.net/elite-daily/2017/05/08110137/Obama-Wiping-Sweat.jpg",
+  WAVE:
+    "https://electronicsforu.com/wp-contents/uploads/2017/09/high-frequency.jpg",
+  OLD_RADIO:
+    "https://d2t1xqejof9utc.cloudfront.net/screenshots/pics/979f9ddfb563b0f9fd48c4508d3f7784/large.JPG",
+  PENDULUM:
+    "https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/vfPFP3W/newton-cradle-pendulum-balls-swinging-back-and-forth-in-a-loop_eju4kaoe__F0000.png",
+  GREEN_CIRCLES:
+    "https://previews.123rf.com/images/headoff/headoff1004/headoff100400001/6729797-a-colourful-green-process-circular-arrow-illustration.jpg",
+
+  MAN_LIGHTING_FIRE:
+    "https://images.newscientist.com/wp-content/uploads/2017/07/11230125/nationalgeographic_2167698-800x533.jpg",
+
+  GUY_IN_DESSERT:
+    "http://www.theprepperjournal.com/wp-content/uploads/2014/02/WillToSurvive.jpg",
+
+  PEOPLE_RUNNING_AWAY:
+    "http://pix.avaxnews.com/avaxnews/4e/bb/0002bb4e_medium.jpeg",
+
+  PERCENTAGE_GOING_UP:
+    "https://www.vancouverrealestatepodcast.com/wp-content/uploads/2018/10/interest-rates-e1541025967190.jpg",
+
+  GIRL_NOT_INTERESTED:
+    "https://www.themodernman.com/wp-content/uploads/woman-pretending-to-reject-a-guy-to-test-his-confidence.jpg",
+
+  HOUSE_INTEREST_RATE:
+    "https://capitalmortgages.com/wp-content/uploads/2018/06/Home-and-Interest-Rates.jpg",
+  BORED_BOY:
+    "https://qph.fs.quoracdn.net/main-qimg-085c469e16ef90f56414e88889a0d81c",
+  FRESHCO:
+    "https://i1.wp.com/media-chrisdca.netdna-ssl.com/wp-content/uploads/2018/07/freshco.jpg?resize=600%2C400&ssl=1",
+  TROPICAL_PLAZA:
+    "https://whereinja.files.wordpress.com/2012/12/img-20120922-01976.jpg",
+
+  LEES_FOOD_FAIR:
+    "https://businessviewcaribbean.com/wp-content/uploads/2018/04/Boulevard-Centre-16-of-141-1170x442.jpg",
+
+  PILLS_FORMING_DOLLAR_SIGN:
+    "https://i.cbc.ca/1.4517667.1517608995!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/pills-in-dollar-sign.jpg",
+
+  FRUITS_AND_VEGETABLES:
+    "https://www.merriam-webster.com/assets/mw/images/article/art-wap-landing-mp-lg/healthy-healthful-2150-863fd74ccc800624767afe34c1c56b3a@1x.jpg",
+
+  PLATE_WITH_FRUITS_AND_VEGETABLES:
+    "https://jeanhailes.org.au/images/made/contents/images/Core_business_areas/Healthy_living/JHkitchen_635_350_s_c1.png",
+
+  DOCTOR_WITH_CHILD:
+    "https://www.hopkinsmedicine.org/sebin/j/t/doctor_patient_nurse_640.jpg",
+
+  BABY_SLEEPING:
+    "https://cdn.newsapi.com.au/image/v1/d44ce2031d9b6fc82741be1ea7197221"
 };
