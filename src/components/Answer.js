8865ab265055c076581639d297a4d15ba373d6ec
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-12-06 17:52:08 -0500

    Added some styles for Anaswer

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 
 import PropTypes from "prop-types";
 
+import "./Answer.css";
+
 export default class Answer extends Component {
   render() {
     const { length, selectedChoices } = this.props;
     const answers = [];
     const numSelectedChoices = selectedChoices.length;
     for (let i = 0; i < length; i += 1) {
-      const text = i < numSelectedChoices ? selectedChoices[i].letter : "";
+      const text = i < numSelectedChoices ? selectedChoices[i].letter : " ";
       answers.push(<span className="answer-letter">{text}</span>);
     }
     return <div className="answer">{answers}</div>;
