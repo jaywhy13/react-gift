b516abd578ff1899edef636d19ed51d177fe3e49
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-12-09 21:10:47 -0500

    Disabled some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: ["airbnb", "prettier"],
   rules: {
     "react/prefer-stateless-function": 0,
-    "react/jsx-filename-extension": 0
+    "react/jsx-filename-extension": 0,
+    "react/prop-types": 0
   },
   plugins: ["prettier"]
 };
